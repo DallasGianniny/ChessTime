de21ce8a42b479427fd4e2bfd83673996da993d3
Dallas G <32652704+DallasGianniny@users.noreply.github.com>
2018-10-17 16:55:46 -0700

    removed title/status bar, added locks to some buttons

--- app/src/main/java/com/example/dallas/chesstime/MainActivity.java
+++ app/src/main/java/com/example/dallas/chesstime/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.dallas.chesstime;
 
 import android.os.CountDownTimer;
+import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(AccessibilityNodeInfoCompat.ACTION_NEXT_HTML_ELEMENT, AccessibilityNodeInfoCompat.ACTION_NEXT_HTML_ELEMENT);
+        getWindow().getDecorView().setSystemUiVisibility(3328);
 
         class BetterCountDownTimer {
             private TextView textView;
@@ -83,19 +88,23 @@ public class MainActivity extends AppCompatActivity {
         turnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                long millisUntilFinished = betterCountDownTimer.getMillisUntilFinished();
-                betterCountDownTimer.countDownTimer.cancel();
-                betterCountDownTimer.createCountDownTimer(millisUntilFinished);
-                betterCountDownTimer2.countDownTimer.start();
+                if (!checkIfEmptyText(textView) || !checkIfEmptyText(textView2)) {
+                    long millisUntilFinished = betterCountDownTimer.getMillisUntilFinished();
+                    betterCountDownTimer.countDownTimer.cancel();
+                    betterCountDownTimer.createCountDownTimer(millisUntilFinished);
+                    betterCountDownTimer2.countDownTimer.start();
+                }
             }
         });
         turnButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                long millisUntilFinished = betterCountDownTimer2.getMillisUntilFinished();
-                betterCountDownTimer2.countDownTimer.cancel();
-                betterCountDownTimer2.createCountDownTimer(millisUntilFinished);
-                betterCountDownTimer.countDownTimer.start();
+                if (!checkIfEmptyText(textView) || !checkIfEmptyText(textView2)) {
+                    long millisUntilFinished = betterCountDownTimer2.getMillisUntilFinished();
+                    betterCountDownTimer2.countDownTimer.cancel();
+                    betterCountDownTimer2.createCountDownTimer(millisUntilFinished);
+                    betterCountDownTimer.countDownTimer.start();
+                }
             }
         });
     }
